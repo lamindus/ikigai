c629e41cd6800fe89c87087bd53545bdda1843c1
lam <lamindus.97@gmail.com>
2020-08-28 11:51:49 +0300

    create new files and add new img; complete projects.html

---
+++ js/main.js
@@ -0,0 +1,6 @@
+$(function(){
+    $('.items').slick({
+        prevArrow: '<button class="slick-prev"><img src="img/prev.svg"></button>',
+        nextArrow: '<button class="slick-next"><img src="img/next.svg"></button>',
+    });
+});
