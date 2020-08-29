425bf0c9e6c336ac858dcf85c658229d316b0190
lam <lamindus.97@gmail.com>
2020-08-29 13:51:39 +0300

    clean directory img and complete team.html

--- js/main.js
+++ js/main.js
@@ -1,6 +1,33 @@
-$(function(){
-    $('.items').slick({
-        prevArrow: '<button class="slick-prev"><img src="img/prev.svg"></button>',
-        nextArrow: '<button class="slick-next"><img src="img/next.svg"></button>',
-    });
-});
+let projects = document.querySelectorAll('.item');
+function next(items){
+    let cur;
+    for(let i = 0; i < items.length; i += 1){
+        if(items[i].classList.contains('active-item')){
+            cur = i;
+        }
+    }
+    if(cur + 1 === items.length){
+        items[cur].classList.remove('active-item');
+        items[0].classList.add('active-item');
+    }
+    else{
+        items[cur].classList.remove('active-item');
+        items[cur + 1].classList.add('active-item');
+    }
+};
+function prev(items){
+    let cur;
+    for(let i = 0; i < items.length; i += 1){
+        if(items[i].classList.contains('active-item')){
+            cur = i;
+        }
+    }
+    if(cur - 1 === -1){
+        items[cur].classList.remove('active-item');
+        items[items.length - 1].classList.add('active-item');
+    }
+    else{
+        items[cur].classList.remove('active-item');
+        items[cur - 1].classList.add('active-item');
+    }
+};
